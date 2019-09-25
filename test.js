106d6e86ba47b17b2e811e5c23862591ec5fe0e5
b16johso <37793460+b16johso@users.noreply.github.com>
2019-09-25 10:16:28 +0200

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', function() {
-    // your code here
-    alert("Nu kommer Figge Ferrum i egen hög person");
-
-	document.getElementById("jform_title").value = "Kräftkalas, kräftkalas";
-  	
-  	var textEditor = document.getElementById("jform_articletext_ifr").contentWindow.document.getElementById("tinymce");
-  	textEditor.getElementByTagName("P")[0].innerHTML = "Kräftkalas, kräftkalas, nu är det kräftkalas!";
-}, false);
